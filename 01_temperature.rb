31685516edbe5e5694512790c7c53e6951098c3f
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-14 15:34:48 +0100

    Add files via upload

---
+++ 01_temperature.rb
@@ -0,0 +1,14 @@
+# Ftoc_formula
+# ftoc = (T(f) - 32) * 5/9]
+
+def ftoc(temp)
+  return (temp-32)*5/9
+end
+
+
+#Ctof_formula"
+# ctof = (T(c)*9)/5 +32
+
+def ctof(temp)
+  return temp*9/5.to_f + 32
+end
